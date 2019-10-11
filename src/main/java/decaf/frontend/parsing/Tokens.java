879b56378857f9076a61da1ecfaf53f72e15dc42
whhh <1372925742@qq.com>
2019-10-11 15:12:44 +0800

    jflex Tokens.java jacc

--- src/main/java/decaf/frontend/parsing/Tokens.java
+++ src/main/java/decaf/frontend/parsing/Tokens.java
@@ -35,6 +35,8 @@ public interface Tokens {
     int GREATER_EQUAL = 28;
     int EQUAL = 29;
     int NOT_EQUAL = 30;
+    int ABSTRACT = 31;
+    int VAR = 32;
 
     // MUST use ASCII code to encode a single-character token.
     // '!' (code=33)
